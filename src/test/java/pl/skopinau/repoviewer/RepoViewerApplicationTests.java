73d7961e247a35ff6a6931d1255a3be2f7b97bbd
Dzmitry Skopinau <dimaskop18@gmail.com>
2023-08-23 13:40:48 +0200

    Initial commit

---
+++ src/test/java/pl/skopinau/repoviewer/RepoViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.skopinau.repoviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
